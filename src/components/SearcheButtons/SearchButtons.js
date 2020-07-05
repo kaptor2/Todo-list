389dfc0ae95f8fb13e06de3bb605c91ccd4d204a
Kostya <kaptor2@gmail.com>
2020-07-05 20:32:30 +0300

    the app is ready. Native and without a single library. The parent component is super-long (150 rows). We must remember to hide it so that no one will see it.

--- src/components/SearcheButtons/SearchButtons.js
+++ src/components/SearcheButtons/SearchButtons.js
@@ -2,12 +2,30 @@ import React, { Component } from "react";
 import "./SearchButtons.css";
 
 export class SearchButtons extends Component {
+
+  buttons = [
+    {name: 'all', type: 'All'},
+    {name: 'active', type: 'Active'},
+    {name: 'done', type: 'Done'}
+  ]
+
   render() {
+
+    const { filter, onFilter } = this.props;
+    
+    const button = this.buttons.map( ({ name, type }) => {
+      const isActive = filter === name ? "btn-elegant" : "btn-light";
+      return <button 
+        key={name} 
+        className={`SearchButton btn ${isActive}`}
+        onClick={() => onFilter(name)}>
+        { type }
+      </button>
+    });
+
     return (
       <>
-        <button className="SearchButton btn btn-elegant">All</button>
-        <button className="SearchButton btn btn-light">Active</button>
-        <button className="SearchButton btn btn-light">Done</button>
+        {button}
       </>
     );
   }
